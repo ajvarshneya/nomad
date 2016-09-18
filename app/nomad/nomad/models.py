94bd3450e75df0f5188aacbd576c31b6df438b31
Spencer Gennari <sdgennari@gmail.com>
2016-09-17 22:04:33 -0400

    Added skeleton models.py

---
+++ app/nomad/nomad/models.py
@@ -0,0 +1,16 @@
+from django.db import models
+
+class User(models.Model):
+	pass
+
+class Listing(models.Model):
+	pass
+
+class Reservation(models.Model):
+	pass
+
+class Review(models.Model):
+	pass
+
+class Tag(models.Model):
+	pass
