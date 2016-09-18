156f13d85f28d32726f93a68426119fb67d3cff8
aj <aj@d-172-25-99-218.dhcp.virginia.edu>
2016-09-17 22:15:18 -0400

    Made a user model

--- app/nomad/nomad/models.py
+++ app/nomad/nomad/models.py
@@ -1,7 +1,16 @@
 from django.db import models
 
 class User(models.Model):
-	pass
+    first_name = models.CharField(max_length=50)
+    last_name = models.CharField(max_length=50)
+    email = models.EmailField()
+    password = models.CharField(max_length=100)
+    username = models.CharField(max_length=50)
+    creditcard = models.CharField(max_length=17)
+    street = models.CharField(max_length=100)
+    city = models.CharField(max_length=50)
+    country = models.CharField(max_length=50)
+    zipcode = models.CharField(max_length=9)
 
 class Listing(models.Model):
 	pass
