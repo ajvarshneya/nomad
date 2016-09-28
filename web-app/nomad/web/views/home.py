2f65e883dccdce4498218c418c9e182a4a77c530
David Zhang <davidzhang@d-172-25-99-210.dhcp.virginia.edu>
2016-09-27 20:32:16 -0400

    Changed views to be directory structure.

--- web-app/nomad/web/views/home.py
+++ web-app/nomad/web/views/home.py
@@ -4,6 +4,9 @@ from django.template import loader
 def index(request):
 	template = loader.get_template('web/index.html')
 	context = {
-		'message': "hacking intensifies!",
-	}
-	return HttpResponse(template.render(context, request))	
+		'message': 'dave is the best',
+	}	
+	return HttpResponse(template.render(context, request))
+
+
+	
