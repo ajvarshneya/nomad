6fa8eb85f26d42cb45293e09caf055d3a990c09a
aj <aj@d-172-25-87-38.dhcp.virginia.edu>
2016-09-29 14:21:29 -0400

    Added listings index, set up detail links

--- web-app/nomad/web/urls.py
+++ web-app/nomad/web/urls.py
@@ -4,6 +4,10 @@ from django.contrib import admin
 from . import views
 
 urlpatterns = [
-    url(r'^$', views.index, name='index'),
+    url(r'^$', views.home.index, name='index'),
     url(r'^listings/$', views.listings.index, name='listings-index'),
+    url(r'^listings/(?P<listing_id>[0-9]+)/$', views.listings.detail, name='listings-detail'),
 ]
+
+
+
--- web-app/nomad/web/views/listings.py
+++ web-app/nomad/web/views/listings.py
@@ -1,12 +1,20 @@
+import urllib.request
+import urllib.parse
+import json
+
 from django.http import HttpResponse
 from django.template import loader
 
 def index(request):
 	template = loader.get_template('web/listings.html')
-	context = {
-		'listings': ["a", "b", "c", "d"],
-	}	
-	return HttpResponse(template.render(context, request))
 
+	models_request = urllib.request.Request('http://exp-api:8000/exp/api/v1/listings')
+	json_response = urllib.request.urlopen(models_request).read().decode('utf-8')
+	response = json.loads(json_response)
 
+	if response["ok"]:
+		context = {"listings" : response["result"]}
+		return HttpResponse(template.render(context, request))
 	
+def detail(request, listing_id):
+	return HttpResponse("listing_id " + listing_id)
