59101e06678fd524fda39f08897ffe92df861778
Spencer Gennari <sdgennari@gmail.com>
2016-09-29 17:56:45 -0400

    Added listing detail template

--- web-app/nomad/web/views/listings.py
+++ web-app/nomad/web/views/listings.py
@@ -17,4 +17,45 @@ def index(request):
 		return HttpResponse(template.render(context, request))
 	
 def detail(request, listing_id):
-	return HttpResponse("listing_id " + listing_id)
+	template = loader.get_template('web/listing-detail.html')
+
+	# Placeholder response data since exp layer not built out yet
+	response = {
+		"ok": True,
+		"result": {
+			"zipcode": "12345",
+			"title": "Beach House",
+			"id": 1,
+			"street": "beach street",
+			"baths": "4.5",
+			"user": {
+				"zipcode": "10118",
+				"email": "ironman@marvel.com",
+				"username": "ironman",
+				"last_name": "Stark",
+				"first_name": "Tony",
+				"street": "350 5th Ave",
+				"city": "New York",
+				"country": "US"
+			},
+			"city": "beach city",
+			"country": "US",
+			"beds": 4,
+			"reviews": [
+			],
+			"reservations": [
+				{
+					"start_date": "2016-09-17T10:00:00Z",
+					"id": 4,
+					"user": 3,
+					"listing": 1,
+					"is_available": False,
+					"end_date": "2016-09-21T19:00:00Z"
+				}
+			]
+		}
+	}
+
+	if response["ok"]:
+		context = {"listing": response["result"]}
+		return HttpResponse(template.render(context, request))
