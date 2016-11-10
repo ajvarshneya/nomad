e64811980255714c58a5a3f66e6b70c660e627cd
Spencer Gennari <sdgennari@gmail.com>
2016-11-09 23:25:53 -0500

    Fixed bug in ES load fixtures script when migrations are made

--- exp-app/load-search-fixtures.py
+++ exp-app/load-search-fixtures.py
@@ -3,6 +3,7 @@ import urllib.parse
 import json
 import requests
 import time
+import sys
 
 from elasticsearch import Elasticsearch
 
@@ -12,17 +13,20 @@ from elasticsearch import Elasticsearch
 #	and the database into the search index.
 # --------------------------------------------------------------------------------
 
+# Wait for other containers
+# Note: Use a large amount of time to allow for migrations and ES config
+time.sleep(45)
+print("Loading DB data into search index...")
+sys.stdout.flush()
+
 # Listings model API call
 index_request = urllib.request.Request('http://models-api:8000/models/api/v1/listings/')
 json_index_response = urllib.request.urlopen(index_request).read().decode('utf-8')
 index_response = json.loads(json_index_response)
 
-# Wait for ES container
-time.sleep(20)
-
-es = Elasticsearch(['es'])
 # Manually add the items to ElasticSearch
+es = Elasticsearch(['es'])
 for listing in index_response["result"]:
 	es.index(index='listing_index', doc_type='listing', id=listing['id'], body=listing)
 es.indices.refresh(index='listing_index')
-print("Done adding listings from DB to index.")
+print("Done adding listings from DB to index")
