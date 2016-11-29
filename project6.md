33731f072fbeac0dda34acf33cfd383061ef0d7a
Spencer Gennari <sdgennari@gmail.com>
2016-11-29 13:24:22 -0500

    Added basic implementation of selenium tests

---
+++ project6.md
@@ -0,0 +1,18 @@
+# Potential Resources for Project 6
+
+## Selenium
+- http://www.seleniumhq.org/
+- http://agiliq.com/blog/2014/09/selenium-testing/
+- https://medium.com/@unary/django-views-automated-testing-with-selenium-d9df95bdc926#.nxqg58i9o
+
+## Digital Ocean
+- https://www.digitalocean.com/community/tutorials/how-to-create-your-first-digitalocean-droplet-virtual-server
+- https://www.digitalocean.com/community/tutorials/how-to-install-and-use-docker-compose-on-ubuntu-14-04
+- https://www.digitalocean.com/community/tutorials/how-to-install-wordpress-and-phpmyadmin-with-docker-compose-on-ubuntu-14-04
+- https://docs.docker.com/compose/environment-variables/
+- Digital Ocean Credits:
+	- https://education.github.com/pack/offers
+
+## Travis
+- https://docs.travis-ci.com/user/docker/
+- https://docs.travis-ci.com/user/for-beginners
--- web-app/nomad/web/tests.py
+++ web-app/nomad/web/tests.py
@@ -1,6 +1,61 @@
-from django.test import TestCase
+from django.test import TestCase, LiveServerTestCase
 from django.core.urlresolvers import reverse
 from django.test.client import RequestFactory
+
+from selenium import webdriver
+from selenium.webdriver.common.desired_capabilities import DesiredCapabilities
+from selenium.webdriver.common.keys import Keys
+
+import os
+import socket
+
+os.environ['DJANGO_LIVE_TEST_SERVER_ADDRESS'] = '0.0.0.0:8004'
+
+# Integration test setup base from StackOverflow:
+# http://stackoverflow.com/questions/32408429/running-django-tests-with-selenium-in-docker
+
+# Use LiveServerTestCase to render the html
+class IntegrationTests(LiveServerTestCase):
+	live_server_url = 'http://{}:8004'.format(
+	    socket.gethostbyname(socket.gethostname())
+	)
+
+	def format_live_server_url(self, web_url):
+		return self.live_server_url + reverse(web_url)
+
+	def setUp(self):
+		# settings.DEBUG = True
+		self.selenium = webdriver.Remote(
+			command_executor="http://selenium:4444/wd/hub",
+			desired_capabilities=DesiredCapabilities.CHROME
+		)
+
+	def tearDown(self):
+		self.selenium.quit()
+		super().tearDown()
+
+	def test_login(self):
+		selenium = self.selenium
+
+		# Get login screen
+		url = self.format_live_server_url("web:auth-login")
+		selenium.get(url)
+
+		# Find form elements
+		username = selenium.find_element_by_id('id_username')
+		password = selenium.find_element_by_id('id_password')
+		submit = selenium.find_element_by_xpath("//input[@type='submit']")
+
+		# Submit the login form
+		username.send_keys('ironman')
+		password.send_keys('password')
+		submit.send_keys(Keys.RETURN)
+
+		# Check that the user is signed in
+		# print(selenium.page_source)
+		self.assertIn('Logout', selenium.page_source)
+		self.assertIn('User', selenium.page_source)
+		self.assertNotIn('Login', selenium.page_source)
 
 # Create your tests here.
 class HomeViewTests(TestCase):
@@ -11,7 +66,6 @@ class HomeViewTests(TestCase):
 		response = self.client.get(reverse("web:index"))
 		self.assertEqual(response.status_code, 200)
 		self.assertContains(response, "Nomad")
-		self.assertContains(response, "Not All Those Who Wander Are Lost")
 
 class ListingViewTests(TestCase):
 	def test_index_view_valid(self):
